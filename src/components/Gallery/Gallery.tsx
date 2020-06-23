b6e4b54288c1ba7f938456f3a7b7f5b9f6b518de
Thibault Goehringer <tgoehringer@gmail.com>
2020-06-23 15:45:44 +0200

    refractored Gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,7 @@
-import { h, Component } from 'preact';
+import { h, FunctionalComponent } from 'preact';
+import { useState }  from 'preact/hooks';
 import config from '../../config';
-import IconExtend from './IconExtend/IconExtend';
+import IconExtend from '../IconExtend/IconExtend';
 import './gallery.css';
 
 interface Props {
@@ -11,61 +12,55 @@ interface State {
     currentImgIndex: number
 }
 
-class Gallery extends Component<Props, State> {
-    state: State = { currentImgIndex: 0 };
+const Gallery: FunctionalComponent<Props> = ({ images }: Props) => {
+    const [currentImgIndex, setCurrentImgIndex] = useState<State['currentImgIndex']>(0);
 
-    handleOnClick = () => {
-        const { images } = this.props;
-        const { currentImgIndex } = this.state;
+    const handleOnClick = () => {
         const newImgIndex = currentImgIndex + 1 < images.length
             ? currentImgIndex + 1
             : 0;
-        this.setState({ currentImgIndex: newImgIndex });
-    }
+        setCurrentImgIndex(newImgIndex);
+    };
 
-    render() {
-        const { images } = this.props;
-        const { currentImgIndex } = this.state;
-        const currentImgPath = images[currentImgIndex]?.path ?? '';
-        return (
-            <div
-                className="gallery"
-                style={{ cursor: images.length > 1 ? 'pointer' : '' }}
-            >   {images.length > 1 && (
-                    <div className="gallery-multiple">
-                        {images.map((image, i) => (
-                            <img
-                                className="gallery-multiple-img"
-                                onClick={this.handleOnClick}
-                                src={`${config.baseUrl}${image.path}`}
-                                style={{ display: i === currentImgIndex ? "block" : "none" }}
-                            />
-                        ))}
+    const currentImgPath = images[currentImgIndex]?.path ?? '';
+    return (
+        <div
+            className="gallery"
+            style={{ cursor: images.length > 1 ? 'pointer' : '' }}
+        >   {images.length > 1 && (
+                <div className="gallery-multiple">
+                    {images.map((image, i) => (
+                        <img
+                            className="gallery-multiple-img"
+                            onClick={handleOnClick}
+                            src={`${config.baseUrl}${image.path}`}
+                            style={{ display: i === currentImgIndex ? "block" : "none" }}
+                        />
+                    ))}
+                </div>
+            )}
+            {images.length === 1 && (
+                <img
+                    className="gallery-single-img"
+                    src={`${config.baseUrl}${currentImgPath}`}
+                />
+            )}
+            <div className="gallery-icons">
+                <a 
+                    className="gallery-extend" 
+                    href={`${config.baseUrl}${currentImgPath}`} 
+                    target="_blank"
+                >
+                    <IconExtend/>
+                </a>
+                {images.length > 0 && (
+                    <div className="gallery-counter">
+                        {currentImgIndex + 1} / {images.length}
                     </div>
                 )}
-                {images.length === 1 && (
-                    <img
-                        className="gallery-single-img"
-                        src={`${config.baseUrl}${currentImgPath}`}
-                    />
-                )}
-                <div className="gallery-icons">
-                    <a 
-                        className="gallery-extend" 
-                        href={`${config.baseUrl}${currentImgPath}`} 
-                        target="_blank"
-                    >
-                        <IconExtend/>
-                    </a>
-                    {images.length > 0 && (
-                        <div className="gallery-counter">
-                            {currentImgIndex + 1} / {images.length}
-                        </div>
-                    )}
-                </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Gallery;
