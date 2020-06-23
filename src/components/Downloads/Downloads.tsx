bd3526cd72c7cc65f0113e4f2e8c8fbeb753f763
Thibault Goehringer <tgoehringer@gmail.com>
2020-06-23 15:39:59 +0200

    converted Downloads to FC

--- src/components/Downloads/Downloads.tsx
+++ src/components/Downloads/Downloads.tsx
@@ -1,41 +1,47 @@
-import { h, Component } from 'preact';
+import { h, FunctionComponent } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 import config from '../../config';
 import { loadJson } from '../../utils/jsonLoader';
 import './Downloads.css';
 
 interface State {
-    downloads: { path: string, title: string }[]
+    downloads: Array<{ path: string, title: string }>
     error: string;
     isError: boolean;
     isLoading: boolean;
 }
 
-class Downloads extends Component<{}, State> {
-    state: State = { downloads: [], error: '', isError: false, isLoading: true };
-
-    componentDidMount() {
+const Downloads: FunctionComponent = () => {
+    const [downloads, setDownloads] = useState<State['downloads']>([]);
+    const [error, setError] = useState<State['error']>('');
+    const [isError, setIsError] = useState<State['isError']>(false);
+    const [isLoading, setIsLoading] = useState<State['isLoading']>(true);
+    
+    useEffect(() => {
         loadJson(`${config.baseUrl}${config.path.getDownloads}`)
             .then((response: any) => {
                 const downloads = response.entries.map((entry: any) => ({
                     path: entry?.path ?? '',
                     title: entry?.title ?? '',
                 }))
-                this.setState({ isLoading: false, downloads });
+                setDownloads(downloads);
+                setIsLoading(false);
             })
             .catch((error: string) => {
-                this.setState({ isLoading: false, isError: true, error });
+                setError(error);
+                setIsError(true);
+                setIsLoading(false);
             })
-    }
-    render() {
-        const { downloads } = this.state;
-        return (
-            <div className="site-header-downloads">
-                {downloads.map(file => (
-                    <a className="site-header-download-btn" href={`${config.baseUrl}/${file.path}`} target="_blank">{file.title}</a>
-                ))}
-            </div>
-        );
-    }
+    }, []);
+
+    return (
+        <div className="site-header-downloads">
+            {!isLoading && !isError && downloads.map(file => (
+                <a className="site-header-download-btn" href={`${config.baseUrl}/${file.path}`} target="_blank">{file.title}</a>
+            ))}
+            {!isLoading && isError && error}
+        </div>
+    );
 }
 
 export default Downloads;
