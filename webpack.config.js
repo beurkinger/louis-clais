ce3259ab6c880fd74df142d8362bcf6657c0abba
Thibault Goehringer <tgoehringer@gmail.com>
2020-08-26 20:03:01 +0200

    Add linting and post-css

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,60 @@
+const Webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".js", ".jsx", ".ts", ".tsx"]
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: [{ loader: 'ts-loader' }]
+        use: [{ loader: 'ts-loader' }],
       },
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
-        loader: "source-map-loader"
+        loader: 'source-map-loader',
       },
       {
         test: /\.css$/,
-        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          '@teamsupercell/typings-for-css-modules-loader',
+          {
+            loader: 'css-loader',
+            // options: { modules: true },
+          },
+          'postcss-loader',
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg|png|jpg|svg)$/,
-        use: [{ 
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'assets/',
-          }
-         }]
-      }
-    ]
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
+    new Webpack.WatchIgnorePlugin([/css\.d\.ts$/]),
     new HtmlWebpackPlugin({
       template: __dirname + '/src/index.html',
-      title: 'Preact Boilerplate'
+      title: 'Louis Clais',
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
   ],
   devServer: {
     contentBase: __dirname + '/public',
     historyApiFallback: true,
     publicPath: '/',
-  }
+  },
 };
