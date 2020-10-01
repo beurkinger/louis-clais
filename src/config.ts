6999dc8c9bddcb878fe4a69d3a49d64c90b00a9c
Thibault Goehringer <tgoehringer@gmail.com>
2020-10-01 23:59:47 +0200

    Update index.html

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export default {
-    baseUrl: "http://localhost:8888",
-    path: {
-        getArticles:  "/louis-backend/api/collections/get/articles",
-        getDownloads: "/louis-backend/api/collections/get/downloads",
-        getHeader: "/louis-backend/api/singletons/get/main",
-    }
+  baseUrl: 'http://localhost:8888',
+  path: {
+    getArticles: '/louis-backend/api/collections/get/articles',
+    getDownloads: '/louis-backend/api/collections/get/downloads',
+    getHeader: '/louis-backend/api/singletons/get/main',
+  },
 };
